068b73ddd12cc8d052fff73a49a63ae1cd0b885b
“Farashi <farashi25@gmail.com>
2017-09-07 18:01:33 -0400

    First Commit

---
+++ javaFun/Casting.java
@@ -0,0 +1,16 @@
+class Casting {
+  public static void main(String[] args) {
+    double d  =35.25;
+    double dd = 35.99;
+    float f = 35;
+    // casting the double d into a int
+    int i = (int) d;
+    // casting the double dd into a int
+    int ii = (int) dd;
+    //casting the float f into a int
+    int iii = (int) f;
+    System.out.println(i);
+    System.out.println(ii);
+    System.out.println("the number is: " + f);
+  }
+}
---
+++ java_oop/ProjectClassTest.java
@@ -0,0 +1,5 @@
+public class ProjectTest{
+  public static void main(String[] args) {
+    Project project = new Project("P1", "Desc1", 10.00)
+  }
+}
