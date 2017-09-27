ee29762a452fa695568025b9621d095ffb94cabd
“Farashi <farashi25@gmail.com>
2017-09-27 13:06:41 -0400

    updates

---
+++ counter/src/main/java/com/frogers/counter/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.frogers.counter.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public String index(HttpSession session){
+		if(session.getAttribute("count") == null){
+			Integer count = 0;
+			session.setAttribute("count", count);
+		}
+		Integer count = (Integer) session.getAttribute("count");
+		session.setAttribute("count", count+1);
+		
+		return "index";
+}
+	@RequestMapping("/counter")
+	public String counter(){
+		
+		return "counting";
+}
+
+	
+}
---
+++ counter/src/main/webapp/WEB-INF/counting.jsp
@@ -0,0 +1,4 @@
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
+    <h2>Thank you for visiting <a href ="/">The welcome user page</a> <c:out value="${count}"/></h2>
+    
+    <a href="/">Would you like to test another visit?</a> 
